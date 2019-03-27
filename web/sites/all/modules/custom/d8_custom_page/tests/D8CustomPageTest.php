4956a5975bd2c73bd9b260a41e07284ff8ece068
Tony Taylor <tony@tonetaylor.com>
2019-03-27 08:56:07 -0500

    added sql dump

--- web/sites/all/modules/custom/d8_custom_page/tests/D8CustomPageTest.php
+++ web/sites/all/modules/custom/d8_custom_page/tests/D8CustomPageTest.php
@@ -11,28 +11,32 @@ declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
 
 class D8CustomPageTest extends TestCase {
-  /*protected function _getRoot() {
+  public static function _webRoot() {
     return dirname(dirname(dirname(dirname(dirname(dirname(dirname(__FILE__)))))));
   }
-  protected function _bootStrap() {
-    define('DRUPAL_ROOT', $this->_getRoot());
+
+  public static function _bootstrap() {
+    define('DRUPAL_ROOT', self::_webRoot());
     require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
     $_SERVER['REMOTE_ADDR'] = '127.0.0.1';
-    // Bootstrap Drupal.
+      // Bootstrap Drupal.
     drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
-  }*/
+  }
 
-  /*public function setUp() {
-    //$this->_bootStrap();
-    //drupal_load('module', 'd8_custom_page');
+  public static function setupBeforeClass() {
+    self::_bootstrap();
+  }
+
+  public function setUp() {
+    drupal_load('module', 'd8_custom_page');
     parent::setUp();
-  }*/
+  }
 
-  /*public function testCustomPageFooFunction() {
+  public function testCustomPageFooFunction() {
     $message = 'calling the "foo" function returns an HTML H1 tag containing "bar".';
     $output = d8_custom_page_foo();
-    $this->assertEqual($output, '<h1>bar</h1>', $message);
-  }*/
+    $this->assertEquals($output, '<h1>bar</h1>', $message);
+  }
 
   public function testCustomPageCanary() {
     $message = 'simple addition operation.';
